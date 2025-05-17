af4e13da0f0ff573f6878090647b2f499c7b3ae6
Назар Стецюк <ne1rohhhhh@gmail.com>
2025-05-17 22:49:12 +0300

    second commit

--- script.js
+++ script.js
@@ -8,15 +8,34 @@ const clearDisplay = () => {
     display.value = '';
 }
 
+const deleteLastElement = () => {
+    const currentValue = display.value;
+    if (currentValue.length > 0) {
+        display.value = currentValue.slice(0, -1);
+    } 
+}
+
+document.addEventListener('keydown', (event) => {
+    if (event.key === 'Enter') calculate();
+    if (event.key === 'Backspace') deleteLastElement();
+});
+
 const calculate = () => {
     try {
-        const result = eval(display.value);
+        let expression = display.value;
+
+        if (!/^([0-9+\-*/%.()\s]|sqrt)+$/.test(expression)) {
+            throw new Error('Invalid characters in expression');
+        }
+
+        expression = expression.replace(/(\d+(\.\d+)?)%(\d+(\.\d+)?)/g, '($1 / 100) * $3');
+
+        expression = expression.replace(/sqrt\(/g, 'Math.sqrt(');
+
+        const result = eval(expression);
         display.value = result;
+
     } catch (error) {
         display.value = 'Error';
     }
-}
-
-if (display.value !== '') {
-    
 }
